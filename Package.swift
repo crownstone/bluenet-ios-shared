8b5017f9806596050493c555970affe91d91f140
alexdm0 <alexdemulder@gmail.com>
2021-08-09 12:58:51 +0200

    removed plaforms

--- Package.swift
+++ Package.swift
@@ -5,10 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "BluenetShared",
-    platforms: [
-        .iOS(.v10),
-        .watchOS(.v4),
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
